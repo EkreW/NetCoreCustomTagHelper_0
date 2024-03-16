c67d51bfe6fa81a40d98155e3551d1abaacd3a2b
Ekrem Güngör <ekrmdsgnr@gmail.com>
2024-03-16 17:51:42 +0300

    Add project files.

---
+++ NetCoreCustomTagHelper_0/CustomTagHelpers/MyTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using NetCoreCustomTagHelper_0.Models.ContextClasses;
+using NetCoreCustomTagHelper_0.Models.Entities;
+
+namespace NetCoreCustomTagHelper_0.CustomTagHelpers
+{
+    //Bir class'ın TagHelper olabilmesi icin TagHelper class'ından miras alması gereklidir...
+    //Bu ifade yarattıgımız  element hangi isimle kullanılacak bunu belirtir
+    public class MyTagHelper : TagHelper
+    {
+        NorthwindContext _db;
+
+        public MyTagHelper(NorthwindContext db)
+        {
+            _db = db;
+        }
+        public string Deneme { get; set; }
+        public string TestVerisi { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            List<Product> products = _db.Products.ToList();
+            string result = null;
+            foreach (Product item in products)
+            {
+                result += $"<tr> <td>{item.ProductName} </td> </tr> ";
+            }
+
+
+            //output.Content.SetHtmlContent($"<h1> Hello World {Deneme} </h1>");
+            output.Content.SetHtmlContent(result);
+        }
+    }
+}
